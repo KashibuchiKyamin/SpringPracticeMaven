9faaa78a3ed1f3660599b92b003c7bc639d399be
ymrkm <y.mrkm.0722@gmail.com>
2020-03-27 20:20:33 +0900

    依存関係の修正とパッケージ変更

--- src/main/java/springPracticeMaven/SpringPracticeMavenApplication.java
+++ src/main/java/springPracticeMaven/SpringPracticeMavenApplication.java
@@ -1,4 +1,4 @@
-package SpringPracticeMaven;
+package springPracticeMaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
