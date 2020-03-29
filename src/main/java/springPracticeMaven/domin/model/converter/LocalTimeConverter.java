2314f8805beacf5ec18737c2b327cc076f58aefa
ymrkm <y.mrkm.0722@gmail.com>
2020-03-29 16:44:18 +0900

    AttributeConverter追加

---
+++ src/main/java/springPracticeMaven/domin/model/converter/LocalTimeConverter.java
@@ -0,0 +1,20 @@
+package springPracticeMaven.domin.model.converter;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+import javax.persistence.AttributeConverter;
+
+public class LocalTimeConverter implements AttributeConverter<LocalTime, Time> {
+
+	@Override
+	public Time convertToDatabaseColumn(LocalTime attribute) {
+		return attribute == null ? null: Time.valueOf(attribute);
+	}
+
+	@Override
+	public LocalTime convertToEntityAttribute(Time dbData) {
+		return dbData == null ? null : dbData.toLocalTime();
+	}
+
+}
